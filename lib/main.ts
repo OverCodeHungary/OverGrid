423d938f4d2e3d27d1b8dc0857f90ab941d6e35c
Robert Ignacz <robber@robber.hu>
2024-11-08 00:53:56 +0100

    fix: fixing APP_VERSION bug

--- lib/main.ts
+++ lib/main.ts
@@ -2,8 +2,6 @@ import OverGrid from './OverGrid.vue'
 import { OperatorButtonType, ConfiglessFormatterConfigType, DateFormatterConfigType, HighlightFormatterConfigType, LongTextFormatterConfigType, OperationsFormatterConfigType, StatusFormatterConfigType, TailwindClassFormatterConfigType, AllFormatterConfigTypes, MappingRecordType, OverGridConfig, PossibleLanguages } from './components/model/OverGridConfig';
 import { OrderDirection } from './components/model/Ordering';
 
-declare const __APP_VERSION__: string
-
 export type {
   OperatorButtonType,
   ConfiglessFormatterConfigType,
